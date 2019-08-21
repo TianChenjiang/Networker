71917c27887f565d97cde14833fb99a60e0f5863
CaribouW <1071956678@qq.com>
2019-08-21 21:03:23 +0800

    gin给予前端静态站点，添加docker部署支持.

---
+++ bash/setup.sh
@@ -0,0 +1,5 @@
+docker-compose down
+docker rmi $(docker images | grep "citicup/" | awk '{print $3}')
+docker rmi $(docker images | grep "none" | awk '{print $3}')
+docker network create proxy
+docker-compose up --build -d && docker-compose logs -f
