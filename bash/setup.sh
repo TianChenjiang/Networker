5020e94cdca702baead0d555f38c58f59f7e057d
Caribou.F <33996470+CaribouW@users.noreply.github.com>
2019-09-08 19:59:29 +0800

    Update setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,4 +2,4 @@ docker-compose down
 docker rmi $(docker images | grep "citicup/" | awk '{print $3}')
 docker rmi $(docker images | grep "none" | awk '{print $3}')
 docker network create proxy
-docker-compose up --build -d && docker-compose logs -f &
+docker-compose up --build -d
