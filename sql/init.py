92ac8f489ba85f2374b667fd3131dc6ec6c6cf4a
CaribouW <1071956678@qq.com>
2019-09-11 20:28:05 +0800

    添加公司

--- sql/init.py
+++ sql/init.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 import tushare as ts
 pro = ts.pro_api('53cd3b985c649c978160c6ec04bce24f4fbd2ebcb4673e8f2fba9a43')
-df = pro.new_share(start_date='20180901', end_date='20181018')
+df = pro.stock_company()
 engine = create_engine('mysql+pymysql://root:mysql@127.0.0.1:33006/citicup?charset=utf8')
 df.to_sql(name='company', con=engine, if_exists='append',index=True, index_label='id')
 
