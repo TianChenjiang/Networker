e768d44c517f7672bc85667e87052b821473e99c
CaribouW <1071956678@qq.com>
2019-09-12 10:09:06 +0800

    添加所有数据

--- sql/init.py
+++ sql/init.py
@@ -2,8 +2,9 @@ import tushare as ts
 from sqlalchemy import create_engine
 
 pro = ts.pro_api('53cd3b985c649c978160c6ec04bce24f4fbd2ebcb4673e8f2fba9a43')
-df = pro.stock_company(fields='ts_code,exchange,chairman,manager,'
-                              'secretary,reg_capital,setup_date,province,'
-                              'city,introduction,website,email,office,employees,main_business,business_scope')
+df = pro.stock_basic(
+    exchange='',
+    fields='ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,delist_date')
+
 engine = create_engine('mysql+pymysql://root:mysql@127.0.0.1:33006/citicup?charset=utf8')
-df.to_sql(name='company', con=engine, if_exists='append', index=True, index_label='id')
+df.to_sql(name='stock', con=engine, if_exists='append', index=True, index_label='id')
