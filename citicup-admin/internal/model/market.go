c55697e78d2e7c1bb13e129d061c40ffa037b6d9
CaribouW <1071956678@qq.com>
2019-09-15 10:08:01 +0800

    update-sql

--- citicup-admin/internal/model/market.go
+++ citicup-admin/internal/model/market.go
@@ -2,22 +2,19 @@ package model
 
 import "citicup-admin/schema"
 
+//日线行情
 type Market struct {
 	ID           uint    `gorm:"column:id;AUTO_INCREMENT;primary_key"`
+	Tscode       string  `gorm:"column:ts_code"`
+	Open         float32 `gorm:"column:open"`          //开盘价
+	TradeDate    string  `gorm:"column:trade_date"`    //交易日期
 	Amount       float32 `gorm:"column:amount"`        //成交额（亿）
-	Amplitude    float32 `gorm:"column:amplitude"`     //振幅 公式：(| highest - lowest | / pre_close) * 100% TODO: 这个可以根据已有数据得到的啊？
 	Change       float32 `gorm:"column:change"`        //涨跌额
 	PctChg       float32 `gorm:"column:pct_chg"`       //涨跌幅
 	Close        float32 `gorm:"column:close"`         //收盘价
-	Highest      float32 `gorm:"column:highest"`       //最高价
-	ListAge      float32 `gorm:"column:list_age"`      //市龄
-	Lowest       float32 `gorm:"column:lowest"`        //最低价
-	Name         string  `gorm:"column:name"`          //股票名称
-	Open         float32 `gorm:"column:open"`          //开盘价
-	Pb           float32 `gorm:"column:pb"`            //市净率
-	Pe           float32 `gorm:"column:pe"`            //市盈率
+	Highest      float32 `gorm:"column:high"`          //最高价
+	Lowest       float32 `gorm:"column:low"`           //最低价
 	PreClose     float32 `gorm:"column:pre_close"`     //昨日收盘价
-	TurnoverRate float32 `gorm:"column:turnover_rate"` //换手率
 	Vol          float32 `gorm:"column:vol"`           //成交量
 }
 
@@ -25,19 +22,13 @@ func (m *Market) Model2Schema() (result schema.Market) {
 	result = schema.Market{
 		ID:           m.ID,
 		Amount:       m.Amount,
-		Amplitude:    m.Amplitude,
 		Change:       m.Change,
 		PctChg:       m.PctChg,
 		Close:        m.Close,
 		Highest:      m.Highest,
-		ListAge:      m.ListAge,
 		Lowest:       m.Lowest,
-		Name:         m.Name,
 		Open:         m.Open,
-		Pb:           m.Pb,
-		Pe:           m.Pe,
 		PreClose:     m.PreClose,
-		TurnoverRate: m.TurnoverRate,
 		Vol:          m.Vol,
 	}
 	return
--- sql/init.py
+++ sql/init.py
@@ -2,9 +2,7 @@ import tushare as ts
 from sqlalchemy import create_engine
 
 pro = ts.pro_api('53cd3b985c649c978160c6ec04bce24f4fbd2ebcb4673e8f2fba9a43')
-df = pro.stock_basic(
-    exchange='',
-    fields='ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,delist_date')
 
+df = pro.daily(start_date='20180701', end_date='20180718')
 engine = create_engine('mysql+pymysql://root:mysql@127.0.0.1:33006/citicup?charset=utf8')
-df.to_sql(name='stock', con=engine, if_exists='append', index=True, index_label='id')
+df.to_sql(name='market', con=engine, if_exists='append', index=True, index_label='id')
