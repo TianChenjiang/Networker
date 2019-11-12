2d66684c373692f6615b454c87193c945d933f58
赵文祺 <1027572886a@gmail.com>
2019-11-12 13:11:07 +0800

    修改版本不兼容的问题

--- citicup-model/production/model/deeplearning_model.py
+++ citicup-model/production/model/deeplearning_model.py
@@ -1,6 +1,7 @@
 from model.files import price_scaler, get_info, info_scaler
 import pathlib
 import tensorflow as tf
+from keras.backend import set_session
 from keras.models import load_model
 
 lookback = 48
@@ -9,10 +10,12 @@ DEEP_MODEL_PATH = PATH.joinpath('pledge_company_model.h5')
 
 
 # 加载深度学习模型
-deep_model = load_model(DEEP_MODEL_PATH)
-deep_model._make_predict_function()
+
+sess = tf.Session()
 graph = tf.get_default_graph()
 
+set_session(sess)
+deep_model = load_model(DEEP_MODEL_PATH)
 
 def transform_price(price_df, forecast_close_line):
     price_df = price_df.drop(['ts_code', 'trade_date', 'pre_close'], axis=1)
@@ -31,7 +34,9 @@ def deep_predict(code, forecast_close_line, price_df):
     info_values = get_info(code, forecast_close_line, pledge_price, info_scaler)
 
     global graph
+    global sess
     with graph.as_default():
+        set_session(sess)
         prob = deep_model.predict([price_values, info_values]).ravel()
     return prob[0]
 
