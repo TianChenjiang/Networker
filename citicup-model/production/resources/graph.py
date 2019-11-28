4b231dae2a536dbd43dd99656c75753324aa819e
CaribouW <1071956678@qq.com>
2019-11-28 08:38:03 +0800

    s

--- citicup-model/production/resources/graph.py
+++ citicup-model/production/resources/graph.py
@@ -55,9 +55,7 @@ class Associate(Resource):
 
 
 _line_parser = reqparse.RequestParser() \
-    .add_argument('code', type=str, help='股票代码', required=True) \
-    .add_argument('start_date', type=str, help='开始日期', required=True) \
-    .add_argument('end_date', type=str, help='结束日期', required=True)
+    .add_argument('code', type=str, help='股票代码', required=True)
 
 
 @api.route('/klines')
@@ -66,7 +64,7 @@ class Klines(Resource):
     @api.expect(_line_parser)
     def get(self):
         args = _line_parser.parse_args()
-        code, s_date, e_date = args['code'], args['start_date'], args['end_date']
+        code, s_date, e_date = args['code'], '20190102', '20190830'
         pro = ts.pro_api('53cd3b985c649c978160c6ec04bce24f4fbd2ebcb4673e8f2fba9a43')
         df = pro.query('daily', ts_code=code, start_date=s_date, end_date=e_date)
         res = []
